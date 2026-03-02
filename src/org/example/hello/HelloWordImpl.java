db054f2cf545c5c957c4da684bd5c1d2518c6c67
fread <siansigsavourat@gmail.com>
2026-03-02 10:53:27 +0100

    Porjet init

---
+++ src/org/example/hello/HelloWordImpl.java
@@ -0,0 +1,15 @@
+package org.example.hello;
+
+public class HelloWordImpl {
+
+	/** Component Lifecycle Method */
+	public void stop(){
+	       System.out.println("Good bye World !");
+	}
+	 
+	/** Component Lifecycle Method */
+	public void start(){
+	       System.out.println("Hello World !");
+	}
+
+}
